4f7cc2cb805f62edc8aa241e92138d81fc3f73f6
ti-abhishek <ti.abhishek15@gmail.com>
2022-12-24 19:12:25 +0530

    n

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,25 @@ import React,{useState,useEffect,useRef} from 'react'
 import '../styles/App.css';
 const App = () => {
 
-//code here 
+//code here
+  const [value2, SetValue2] = useState("");
+  const [value, SetValue] = useState("");
+  const newRef = useRef(null);
+
+  const settingValue = () => {
+    SetValue2(value);
+    SetValue("");
+  }
+
+  const handle = () => { }
 
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={} ref={} onChange={}/><br/>
+      <input id="inputEmail" type="text" value={value} ref={newRef} onChange={}/><br/>
       Password
       <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
-      <button id="submitButton" onClick={}>Submit</button><br/>
+      <button id="submitButton" onClick={handle}>Submit</button><br/>
       <p id="emailText">Your Email : {}</p>
       <p id ="passwordText">Your Password : {}</p>
       
